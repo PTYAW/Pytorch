360b06686a649a534923d8fbea05817f1e96d76a
PTYAW <1016107929@qq.com>
2022-08-30 17:34:53 +0800

    增加18_的内容：模型的读取和保存

--- Study/18_VGG分类模型.py
+++ Study/18_VGG分类模型.py
@@ -1,11 +1,11 @@
 #   torchvision.models
 #   使用的数据集：ImageNet
-
+import torch
 import torchvision
 from torch import nn
 
 #   pretrained=True,返回一个在ImageNet中训练好的模型
-#   pretrained=False,仅返回一些信息
+#   pretrained=False,返回一个未经训练的模型
 vgg16_false = torchvision.models.vgg16(pretrained=False)
 vgg16_true = torchvision.models.vgg16(pretrained=True)
 print("vgg16_true")
@@ -18,3 +18,17 @@ train_data = torchvision.datasets.CIFAR10("datset_CIF10", train=True, transform=
 vgg16_true.add_module('add_linear', nn.Linear(1000, 10))
 #   2、直接修改最后一层的结构
 vgg16_false.classifier[6] = nn.Linear(4096, 10)
+
+#   模型的保存
+vgg16 = torchvision.models.vgg16(pretrained=False)
+#   保存方式1:保存了参数和结构
+torch.save(vgg16, "vgg16_method1.pth")
+#   方式1的读取
+# modle = torch.load("vgg16_method1.pth")
+# print(modle)
+
+#   保存方式2:只保存参数(官方推荐)
+torch.save(vgg16.state_dict(), "vgg16_method2.pth")
+#   方式2的读取
+vgg16.load_state_dict(torch.load("vgg16_method2.pth"))
+print(vgg16)
